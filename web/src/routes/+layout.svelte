350fb397563bc8e9dc59ad2325b0b0f43bf61312
orosmatthew <orosmatthew@pm.me>
2023-04-26 12:20:01 -0400

    Add initial web framework

---
+++ web/src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import 'bootstrap-dark-5/dist/css/bootstrap-night.min.css';
+	import { onMount } from 'svelte';
+
+	onMount(async () => {
+		await import('bootstrap');
+	});
+</script>
+
+<slot />
