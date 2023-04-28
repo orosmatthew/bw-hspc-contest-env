a7c73e106bc3b8a54db799658234ffb5c5d543fe
orosmatthew <orosmatthew@pm.me>
2023-04-27 22:26:54 -0400

    Add initial login

---
+++ web/src/routes/login/+page.server.ts
@@ -0,0 +1,18 @@
+import type { Actions } from '@sveltejs/kit';
+import { db } from '$lib/server/prisma';
+
+export const actions = {
+	login: async ({ cookies, request }) => {
+		const data = await request.formData();
+		const username = data.get('username')?.toString();
+		const password = data.get('password')?.toString();
+		const user = await db.user.findUnique({ where: { username: username } });
+		if (!user) {
+			return { success: false };
+		}
+		if (user.password === password) {
+			return { success: true };
+		}
+		return { success: false };
+	}
+} satisfies Actions;
---
+++ web/src/routes/login/+page.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import { enhance } from '$app/forms';
+	import type { ActionData } from './$types';
+	export let form: ActionData;
+</script>
+
+<div class="mt-4 row justify-content-center">
+	<div class="col-4">
+		<h1>Login</h1>
+		<form method="POST" action="?/login" use:enhance>
+			<label for="username_field" class="form-label">Username</label>
+			<input type="text" name="username" class="form-control" id="username_field" />
+
+			<label for="password_field" class="form-label">Password</label>
+			<input type="password" name="password" class="form-control" id="password_field" />
+
+			<div class="mt-2">
+				<button type="submit" class="btn btn-primary">Login</button>
+			</div>
+		</form>
+		<div class="mt-2">
+			{#if form?.success}
+				<div class="alert alert-success" role="alert">Success!</div>
+			{:else if form && !form.success}
+				<div class="alert alert-danger" role="alert">Invalid login</div>
+			{/if}
+		</div>
+	</div>
+</div>
