4b3e40decd117e2a408d64ad97f72dd0a7b5a424
orosmatthew <orosmatthew@pm.me>
2023-08-26 14:19:03 -0400

    Add dark diff theme

--- web/src/routes/admin/submissions/[submissionId]/+page.svelte
+++ web/src/routes/admin/submissions/[submissionId]/+page.svelte
@@ -115,5 +115,56 @@
 	<h3 style="text-align:center">Output</h3>
 	<textarea use:stretchTextarea class="code mb-3 form-control" disabled>{data.output}</textarea>
 	<h3 style="text-align:center">Diff</h3>
-	<div id="diff" />
+	<div id="diff" class="dark-diff" />
 {/if}
+
+<style lang="scss">
+	:global(.dark-diff) {
+		:global(.d2h-code-side-linenumber),
+		:global(.d2h-info),
+		:global(.d2h-emptyplaceholder),
+		:global(.d2h-code-side-emptyplaceholder),
+		:global(.d2h-file-header),
+		:global(.d2h-tag) {
+			background-color: var(--bs-body-bg);
+			color: var(--bs-body-color);
+		}
+		:global(span) {
+			color: var(--bs-body-color);
+		}
+
+		:global(.d2h-file-wrapper) {
+			border-color: var(--bs-border-color);
+		}
+
+		:global(.d2h-file-header) {
+			border-bottom-color: var(--bs-border-color);
+		}
+
+		:global(.d2h-info) {
+			border-color: var(--bs-border-color);
+		}
+
+		:global(.d2h-del) {
+			background-color: var(--bs-danger-border-subtle);
+			border-color: var(--bs-danger);
+		}
+
+		:global(del) {
+			background-color: rgba(210, 85, 97, 0.5);
+		}
+		:global(.d2h-ins) {
+			background-color: var(--bs-success-border-subtle);
+			border-color: var(--bs-success);
+		}
+
+		:global(.d2h-code-side-emptyplaceholder),
+		:global(.d2h-emptyplaceholder) {
+			border-color: var(--bs-border-color);
+		}
+
+		:global(ins) {
+			background-color: rgba(13, 125, 75, 0.5);
+		}
+	}
+</style>
