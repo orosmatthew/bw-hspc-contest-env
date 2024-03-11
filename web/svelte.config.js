2f3b951c409c0917ddc64dd6b4def2adf3e2b2fb
orosmatthew <orosmatthew@pm.me>
2024-03-11 13:08:17 -0400

    Fix web imports

--- web/svelte.config.js
+++ web/svelte.config.js
@@ -8,7 +8,7 @@ const config = {
 	kit: {
 		adapter: adapter(),
 		alias: {
-			"@extensionWeb/*": "../shared/extensionWeb/*"
+			'@extensionWeb/*': '../shared/extensionWeb/*'
 		}
 	},
 	vitePlugin: {
