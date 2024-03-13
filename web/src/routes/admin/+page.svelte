126fa4ed440e34e5c99396bc076a77891a3d3899
orosmatthew <orosmatthew@pm.me>
2024-03-13 18:40:24 -0400

    [Web] Add link to public scoreboard on dashboard

--- web/src/routes/admin/+page.svelte
+++ web/src/routes/admin/+page.svelte
@@ -3,3 +3,42 @@
 </svelte:head>
 
 <h1 style="text-align:center" class="mb-4"><i class="bi bi-speedometer2"></i> Dashboard</h1>
+
+<div class="row">
+	<div class="col-md-6">
+		<a
+			class="card stats-card bg-body-tertiary stats-card-link stats-card-action"
+			draggable="false"
+			href="/public/scoreboard"
+		>
+			<h4><i class="bi bi-link-45deg"></i> Public Scoreboard</h4>
+		</a>
+	</div>
+	<div class="col-md-6 mt-3 mt-md-0"></div>
+</div>
+
+<style>
+	.stats-card {
+		text-align: left;
+		padding: 20px;
+		border-radius: 10px;
+		transform: scale(1);
+		transition: 0.1s;
+		text-decoration: none;
+	}
+	.stats-card-link {
+		color: inherit;
+		text-decoration: none;
+		vertical-align: top;
+		transform: scale(1);
+		transition: 0.1s;
+	}
+
+	.stats-card-link:hover {
+		transform: scale(1.02);
+	}
+
+	.stats-card-action:hover {
+		cursor: pointer;
+	}
+</style>
