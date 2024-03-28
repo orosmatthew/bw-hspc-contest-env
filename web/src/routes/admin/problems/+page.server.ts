ed24779338ecdbaac228639a70af462b14239b94
orosmatthew <orosmatthew@pm.me>
2024-03-28 11:50:01 -0400

    [Web] Sort problems

--- web/src/routes/admin/problems/+page.server.ts
+++ web/src/routes/admin/problems/+page.server.ts
@@ -3,7 +3,7 @@ import { db } from '$lib/server/prisma';
 import type { PageServerLoad } from './$types';
 
 export const load = (async () => {
-	const query = await db.problem.findMany();
+	const query = await db.problem.findMany({ orderBy: { friendlyName: 'asc' } });
 	return {
 		problems: query.map((row) => {
 			return {
