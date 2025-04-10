94c8a566c9146f463b5914e9f75ff9f36c66f8b4
orosmatthew <orosmatthew@pm.me>
2025-04-09 20:08:23 -0400

    Update csproj to net8.0 from net7.0

--- web/src/lib/server/templates.ts
+++ web/src/lib/server/templates.ts
@@ -20,7 +20,7 @@ export const templateCSharpProblemProj = `<Project Sdk="Microsoft.NET.Sdk">
 
 <PropertyGroup>
   <OutputType>Exe</OutputType>
-  <TargetFramework>net7.0</TargetFramework>
+  <TargetFramework>net8.0</TargetFramework>
   <ImplicitUsings>enable</ImplicitUsings>
   <Nullable>enable</Nullable>
 </PropertyGroup>
