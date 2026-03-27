f075f6ea244c06fa3b9c6e3803c2f441309dc250
orosmatthew <mjo1@clevelandmetroparks.com>
2026-03-27 11:28:28 -0400

    [web] Update scoreboard json export

--- web/src/routes/admin/scoreboard/json/[contestId]/+server.ts
+++ web/src/routes/admin/scoreboard/json/[contestId]/+server.ts
@@ -1,8 +1,7 @@
 import { error, json } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
-import { scoreboardData } from '$lib/server/scoreboard-data';
 import z from 'zod';
-import { contestRepo } from '$lib/server/repos';
+import { scoreboardService } from '$lib/server/services';
 
 type ScoreboardExportSchema = {
 	problems: Array<string>;
@@ -17,35 +16,29 @@ type ScoreboardExportSchema = {
 	}>;
 };
 
-export const GET = (async ({ params }) => {
+export const GET: RequestHandler = async ({ params }) => {
 	const contestIdParse = z.coerce.number().int().safeParse(params.contestId);
 	if (!contestIdParse.success) {
 		error(400, { message: 'Invalid contest id' });
 	}
-	const contest = await contestRepo.getById(contestIdParse.data);
-	if (contest === undefined) {
-		error(404, 'Contest not found');
+	const scoreboard = await scoreboardService.getForContest(contestIdParse.data);
+	if (scoreboard === undefined) {
+		error(500, { message: 'Unable to create scoreboard data' });
 	}
-	const data = scoreboardData(contest);
-	const problems = data.contest.problems.map((p) => p.friendlyName);
-	const problemIds = data.contest.problems.map((p) => p.id);
+	const problemIds = scoreboard.contest.problems.map((p) => p.id);
 	const exportData: ScoreboardExportSchema = {
-		problems: problems,
-		teams: data.contest.teams.map((t) => {
-			return {
-				correct: t.solves,
-				name: t.name,
-				points: parseFloat(t.time.toFixed(2)),
-				problems: t.problems
-					.sort((a, b) => problemIds.indexOf(a.id) - problemIds.indexOf(b.id))
-					.map((p) => {
-						return {
-							attempts: p.attempts !== 0 ? p.attempts : null,
-							time: p.min !== undefined ? parseFloat(p.min.toFixed(2)) : null
-						};
-					})
-			};
-		})
+		problems: scoreboard.contest.problems.map((p) => p.friendlyName),
+		teams: scoreboard.contest.teams.map((t) => ({
+			correct: t.solves,
+			name: t.name,
+			points: parseFloat(t.time.toFixed(2)),
+			problems: t.problems
+				.sort((a, b) => problemIds.indexOf(a.id) - problemIds.indexOf(b.id))
+				.map((p) => ({
+					attempts: p.attempts !== 0 ? p.attempts : null,
+					time: p.min !== undefined ? parseFloat(p.min.toFixed(2)) : null
+				}))
+		}))
 	};
 	return json(exportData);
-}) satisfies RequestHandler;
+};
